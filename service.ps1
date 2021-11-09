3c35a312d8307697e24e09fa19deb2f6820b66de
arpitapractise <93905786+arpitapractise@users.noreply.github.com>
2021-11-09 11:35:23 +0000

    script uploaded

---
+++ service.ps1
@@ -0,0 +1,17 @@
+﻿
+
+
+function checkService{
+param($winupdate)
+$getservice = Get-Service -Name $winupdate
+if($getservice.status -ne 'Running'){
+echo "starting service"
+Start-Service -Name wuauserv
+#Send-MailMessage -To '<arpitapractise@gmail.com>' -Subject 'send alter that service was stopped' #need valid SMTP servername to send mail paramater is -SmtpServer
+ }
+elseif($getservice.Status -eq 'Running')
+{
+echo "service is running"
+}
+}
+checkService('wuauserv') # pass any valid service name as agrument 
